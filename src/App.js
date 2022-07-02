e6dc0ac3d875944a8acf90fff8c419fc31a8169b
nao <nurettinakinoral@gmail.com>
2022-07-02 18:34:26 +0300

    Project is done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import emailIcon from './asset/email.svg';
+import locationIcon from './asset/location.svg';
+import phoneIcon from './asset/phone.svg';
+// import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState();
+
+  const handleClick = () => {
+    fetch('https://randomuser.me/api/')
+      .then(response => response.json())
+      .then(data => {
+        setData(data.results);
+        console.log(data.results[0]);
+      });
+  };
+  useEffect(() => {
+    handleClick();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data?.map((per, index) => (
+        <div key={index} className="user-container">
+          <div className="profile">
+            <img src={per.picture.large} alt="user" />
+            <h3>
+              {per.name.title} {per.name.first} {per.name.last}
+            </h3>
+          </div>
+          <div className="email">
+            <img src={emailIcon} alt="email" />
+            <p>{per.email}</p>
+          </div>
+          <div className="phone">
+            <img src={phoneIcon} alt="phone" />
+            <p>{per.cell}</p>
+          </div>
+          <div className="location">
+            <img src={locationIcon} alt="location" />
+            <p>
+              {per.location.city} - {per.location.country}
+            </p>
+          </div>
+          <div className="age">
+            <p>Age: {per.dob.age}</p>
+            <p>Register Date: {per.registered.date.substr(0, 10)}</p>
+          </div>
+        </div>
+      ))}
+
+      <button className="btn-random" onClick={handleClick}>
+        Random User
+      </button>
     </div>
   );
 }
